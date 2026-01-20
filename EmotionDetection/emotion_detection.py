6976a995d1300323133cfedcab9905c543e98c66
Senthil Kumar Arumugam <asenthil2k@yahoo.com>
2026-01-19 20:55:18 -0500

    Refactor after statis analysis

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,9 +6,10 @@ def emotion_detector(text_to_analyse):
     request_body = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json=request_body, headers=header)
     json_response = json.loads(response.text)
-    final_response = json_response["emotionPredictions"]
+    final_response = json_response
     if response.status_code == 200:
         try:
+            final_response = json_response["emotionPredictions"]
             emotion_data = final_response[0]['emotion']
             dominant_emotion = max(emotion_data, key=emotion_data.get)
             emotion_data["dominant_emotion"] = dominant_emotion
@@ -16,6 +17,8 @@ def emotion_detector(text_to_analyse):
         except Exception as e:
             print("Invalid response")
             print(e)
+    elif response.status_code == 400:
+        final_response = {"anger" : None, "disgust": None, "fear": None, "joy": None, "sadness":None, "dominant_emotion": None}
     else:
         final_response = {"anger" : "", "disgust": "", "fear": "", "joy": "", "sadness":"", "dominant_emotion": ""}
     return final_response
--- server.py
+++ server.py
@@ -6,14 +6,15 @@ app = Flask("Emotion Detection")
 @app.route("/emotionDetector")
 def detect_emotion():
     text_to_analyze = request.args.get("textToAnalyze")
-    print(text_to_analyze)
     emotion_response = emotion_detector(text_to_analyze)
-    response =f"""For the given statement, the system response is 
-    'anger': {emotion_response["anger"]}, 'disgust': {emotion_response["disgust"]},
-    'fear': {emotion_response["fear"]}, 'joy': {emotion_response["joy"]},
-    'sadness': {emotion_response["sadness"]}.  The dominant emotion is {emotion_response["dominant_emotion"]}.
-    """
-    return response
+    if emotion_response["dominant_emotion"] is None:
+        return "Invalid text! Please try again!"
+    else:
+        return f"""For the given statement, the system response is
+        'anger': {emotion_response["anger"]}, 'disgust': {emotion_response["disgust"]},
+        'fear': {emotion_response["fear"]}, 'joy': {emotion_response["joy"]},
+        'sadness': {emotion_response["sadness"]}.  The dominant emotion is {emotion_response["dominant_emotion"]}.
+        """
 
 @app.route("/")
 def render_index_page():
